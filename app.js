e11b581b33c3cd45185d553d888c70b74c9afc89
DimensionalDragon <andhikangurah@gmail.com>
2022-04-06 01:17:39 +0800

    Add index html file

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Welcome');
+    res.sendFile(`${__dirname}/index.html`);
 });
 
 app.get('/get_file/:name', (req, res) => {
